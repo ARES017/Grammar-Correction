12d5e9ea441d2eab0a8cf1e99052bda2b4bdef7c
Ananthu <ananthu017@gmail.com>
2022-04-30 11:10:19 +0530

    Updated HTML page layout for grammar correction application. Also implemeneted functions in HTML page to send input text to backend and get back response

--- grammer_correction_app.py
+++ grammer_correction_app.py
@@ -12,24 +12,28 @@ def landing_page():
     
     elif request.method == "POST":
         try:
-            input_sentence = request.form["originalText"]
+            input_sentence = request.json["inputText"]
         except KeyError:
             input_sentence = None
 
         if input_sentence:
-            sentence_list = tokenize_sentences(input_sentence)
-            updated_sentence, corrected_sentece_edit_list = grammer_correct(gf, sentence_list)
+            # sentence_list = tokenize_sentences(input_sentence)
+            return {
+                "payload": input_sentence
+            }
+            # updated_sentence, corrected_sentece_edit_list = grammer_correct(gf, sentence_list)
 
-            if updated_sentence:
-                return render_template('result.html', result=updated_sentence)
-            else:
-                return render_template('result.html', result=input_sentence)
+            # if updated_sentence:
+            #     return render_template('result.html', result=updated_sentence)
+            # else:
+            #     return render_template('result.html', result=input_sentence)
         
         else:
-            return "No input text"
-
+            return {
+                "payload": ""
+            }
 
 if __name__ == '__main__':
     set_seed(1212)
-    gf = Gramformer(models = 1, use_gpu=False) # 1=corrector, 2=detector 
+    # gf = Gramformer(models = 1, use_gpu=False) # 1=corrector, 2=detector 
     app.run(debug=True)
